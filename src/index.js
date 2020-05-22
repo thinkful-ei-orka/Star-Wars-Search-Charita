ac57746ae5af2ca5bc5c5483d1d84a021db9f95f
Charita Nichols <nichols.charita@gmail.com>
2020-05-22 14:41:01 -0400

    changes

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+
+ReactDOM.render(
+  <React.StrictMode.mode>
+    <App />
+  </React.StrictMode.mode>,
+   document.getElementById('root')
+);
